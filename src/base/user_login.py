11d0c260a8ca9d1a0f6d23b16dc34acfd6d07810
samarth1029 <samarthmishra291@gmail.com>
2023-03-30 20:17:38 +0530

    format the code in complaince with PEP8

--- src/base/user_login.py
+++ src/base/user_login.py
@@ -32,25 +32,49 @@ class LoginWindow:
         f_lbl = Label(self.root, image=self.photoimg1, bg='black', borderwidth=0)
         f_lbl.place(x=579, y=125, width=79, height=74)
 
-        get_str = Label(self.root2, text="Get Started", font=("lucida handwriting", 12, "bold"), fg='white', bg="black")
+        get_str = Label(self.root2,
+                        text="Get Started",
+                        font=("lucida handwriting", 12, "bold"),
+                        fg='white',
+                        bg="black")
         get_str.place(x=80, y=85)
 
-        username_lbl = Label(self.root2, text="Username", font=("lucida handwriting", 10), fg='white', bg="black")
+        username_lbl = Label(self.root2,
+                             text="Username",
+                             font=("lucida handwriting", 10),
+                             fg='white',
+                             bg="black")
         username_lbl.place(x=20, y=125)
 
-        self.user_entry = ttk.Entry(self.root2, textvariable=self.username, width=37,
+        self.user_entry = ttk.Entry(self.root2,
+                                    textvariable=self.username,
+                                    width=37,
                                     font=("times new roman", 10))
         self.user_entry.place(x=20, y=145)
 
-        pass_lbl = Label(self.root2, text="Password", font=("lucida handwriting", 10), fg='white', bg="black")
+        pass_lbl = Label(self.root2,
+                         text="Password",
+                         font=("lucida handwriting", 10),
+                         fg='white',
+                         bg="black")
         pass_lbl.place(x=20, y=175)
 
-        self.pass_entry = ttk.Entry(self.root2, textvariable=self.password, width=37, font=("times new roman", 10),
+        self.pass_entry = ttk.Entry(self.root2,
+                                    textvariable=self.password,
+                                    width=37,
+                                    font=("times new roman", 10),
                                     show='*')
         self.pass_entry.place(x=20, y=195)
 
-        login_btn = Button(self.root2, text="Login", command=self.login, width=14, font=("times new roman", 13),
-                           bg="red", fg="white", activeforeground='white', activebackground='red')
+        login_btn = Button(self.root2,
+                           text="Login",
+                           command=self.login,
+                           width=14,
+                           font=("times new roman", 13),
+                           bg="red",
+                           fg="white",
+                           activeforeground='white',
+                           activebackground='red')
         login_btn.place(x=70, y=230)
 
     def login(self):
@@ -61,12 +85,12 @@ class LoginWindow:
             headers = {"Content-type": "application/json"}
             _dict = {
                 "query": f'''
-                            SELECT * FROM `sandbox-381608.registration.registered_users`
+                            SELECT * FROM `sandbox-381608.jis.registered_users`
                             WHERE 
                             email="{self.username.get()}" and 
                             password="{self.password.get()}"
                         ''',
-                "gbq_table_id": "sandbox-381608.user_data.user_profiles",
+                "gbq_table_id": "sandbox-381608.jis.registered_users",
             }
             _response = requests.post(url, headers=headers, json=_dict)
             _result = _response.json()
