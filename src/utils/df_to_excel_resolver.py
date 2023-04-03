c91de09397cc3ae75e14ab05e1755e7a184761ca
samarth1029 <samarthmishra291@gmail.com>
2023-04-03 21:44:39 +0530

    use ExcelWriter from xlxswriter

--- src/utils/df_to_excel_resolver.py
+++ src/utils/df_to_excel_resolver.py
@@ -3,6 +3,5 @@ import pandas as pd
 
 def df_to_excel(df):
     out_path = r"C:\BI\Projects\judiciary-information-system\src\data\query_result.xlsx"
-    writer = pd.ExcelWriter(out_path, engine='xlsxwriter')
-    df.to_excel(writer, sheet_name='Sheet1')
-    writer.save()
+    with pd.ExcelWriter(out_path, mode='w') as writer:
+        df.to_excel(writer, sheet_name='Sheet1')
